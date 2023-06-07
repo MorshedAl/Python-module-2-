e68baf46c356376ba17ce5fcec644f663b3783b4
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-07 20:43:50 +0600

    Add files via upload

---
+++ copy_list.py
@@ -0,0 +1,16 @@
+
+#Make a copy of a list with the copy():
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+#Make a copy of a list with the list():
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+
+
+
+
