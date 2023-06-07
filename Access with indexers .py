d659d4c87b9a7cad65112682940f5ddc144c9e7d
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-07 13:47:28 +0600

    Add files via upload

---
+++ Access with indexers .py
@@ -0,0 +1,57 @@
+
+##Access Items
+List =['a','b','c','d','e','f']
+print(List[1])
+
+#last item of the list:
+List =['a','b','c','d','e','f']
+print(List[-1])
+
+# range if indexes:
+List =['a','b','c','d','e','f']
+print(List[2:-1]) #without last element
+
+List =['a','b','c','d','e','f']
+print(List[0:])
+
+List =['a','b','c','d','e','f']
+print(List[:4])
+
+List =['a','b','c','d','e','f']
+print(List[:])
+
+#negative range indexes:
+List =['a','b','c','d','e','f']
+print(List[-4:-1])
+
+List =['a','b','c','d','e','f']
+print(List[-4:])
+
+
+List =['a','b','c','d','e','f']
+print(List[:-1])
+
+
+#check if item exist:
+
+List =['a','b','c','d','e','f']
+if 'c' in List:
+ print('yes')
+else:
+  print('No')
+
+
+List =['a','b','c','d','e','f']
+if 'w' in List:
+ print('yes')
+else:
+  print('No')
+
+
+
+
+
+
+
+
+
